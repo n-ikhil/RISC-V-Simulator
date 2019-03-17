85870f73fd3f4efc3a0b4f463afd369329f1cd6f
Ramakrishna <2017csb1075@gmail.com>
2019-03-17 13:33:12 +0530

    modified:   LexicalAnalyser.java 	modified:   assembler.java 	modified:   instruction.java 	deleted:    tes_main.java 	new file:   test.s

--- src/cornflakes/assembler/assembler.java
+++ src/cornflakes/assembler/assembler.java
@@ -1,8 +1,181 @@
 package assembler;
+import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
-import assembler.LexicalAnalyser;
+public class assembler
+	{
+	static  String[] registers= new String[]{"x1","x2","x3","x4","x5","x6","x7","x8","x9","x10","x11","x12","x13","x14","x15","x16","x17","x18","x19","x20","21","x22","x23","x24","x25","x26","x27","x28","x29","x30","x31"};
+	/*
+	 * wrapper class for labels
+	 */
+	static class label
+	{
+		int line;
+		String name;
+		label(int line,String name)
+		{
+			this.line=line;
+			this.name=name;
+		}
+	}
+	/*
+	 * wrapper class for generic instruction
+	 */
 
-class assembler
-{
-	
-}
+	static int line_no=0;
+	static ArrayList<label>labels=new ArrayList<label>();
+	static void assemble() throws IOException
+		{
+		File file = new File("test.s");
+		BufferedReader br = new BufferedReader(new FileReader(file));
+		String line;
+			while ((line = br.readLine()) != null)
+				{
+						LexicalAnalyser temp=new LexicalAnalyser(line);
+						if(temp.islabel==true)
+						{
+							label temp1=new label(line_no,temp.label);
+							labels.add(temp1);
+							instruction.add_label(temp.label);
+							if(temp.Tokens.size()==0)
+							{
+								line=br.readLine();
+								LexicalAnalyser temp2=new LexicalAnalyser(line);
+								parse_instruction(temp2);
+							}
+							
+						}
+						else
+						{
+							parse_instruction(temp);
+						}
+						line_no++;
+				}
+		}
+	static void parse_instruction(LexicalAnalyser tokenlist)
+	{
+		String instr=tokenlist.Tokens.get(0);
+		String temp=instruction_type(instr);
+		if(temp==null)
+		{
+			System.out.println("no such instruction is found");
+		}
+		else
+		{
+			if(temp=="r")
+			{
+				
+			}
+		}
+	}
+	static String instruction_type(String instr)
+	{
+		switch (instr)
+		{
+		case "add":
+			return "r";
+		case "sub":
+			return "r";
+		case "sll":
+			return "r";
+		case "slt":
+			return "r";
+		case "sltu":
+			return "r";
+		case "xor":
+			return "r";
+		case "srl":
+			return "r";
+		case "sra":
+			return "r";
+		case "or":
+			return "r";
+		case "and":
+			return "r";
+		case "addw":
+			return "r";
+		case "subw":
+			return "r";
+		case "sllw":
+			return "r";
+		case "srlw":
+			return "r";
+		case "sraw":
+			return "r";
+		case "lb":
+			return "i";
+		case "lh":
+			return "i";
+		case "lw":
+			return "i";
+		case "ld":
+			return "i";
+		case "lbu":
+			return "i";
+		case "lhu":
+			return "i";
+		case "lwu":
+			return "i";
+		case "fence":
+			return "i";
+		case "fence.i":
+			return "i";
+		case "addi":
+			return "i";
+		case "slli":
+			return "i";
+		case "slti":
+			return "i";
+		case "sltiu":
+			return "i";
+		case "xori":
+			return "i";
+		case "srli":
+			return "i";
+		case "srai":
+			return "i";
+		case "ori":
+			return "i";
+		case "andi":
+			return "i";
+		case "addiw":
+			return "i";
+		case "slliw":
+			return "i";
+		case "srliw":
+			return "i";
+		case "sraiw":
+			return "i";
+		case "sd":
+			return "i";
+		case "jalr":
+			return "i";
+		case "sb":
+			return "s";
+		case "sh":
+			return "s";
+		case "sw":
+			return "s";
+		case "auipc":
+			return "u";
+		case "lui":
+			return "u";
+		case "beq":
+			return "sb";
+		case "bne":
+			return "sb";
+		case "blt":
+			return "sb";
+		case "bge":
+			return "sb";
+		case "bltu":
+			return "sb";
+		case "bgeu":
+			return "sb";
+		}
+		return null;
+	}
+	}
