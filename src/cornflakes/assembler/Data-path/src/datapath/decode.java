40e28c89a27dc8c9b4670e7cec007d05a38211ec
yashaswi2000 <31373501+yashaswi2000@users.noreply.github.com>
2019-03-21 00:22:55 +0530

    updates id errors

--- src/cornflakes/assembler/Data-path/src/datapath/decode.java
+++ src/cornflakes/assembler/Data-path/src/datapath/decode.java
@@ -103,7 +103,7 @@ public class decode {
 								imms= array.substring(20 ,31);
 								imm=Integer.parseInt(imms,2);
 							case "110" : // lwu
-								type=7;
+								id=7;
 								type=2;
 								rds= array.substring(7,11);
 								rd=Integer.parseInt(rds,2);
@@ -116,7 +116,7 @@ public class decode {
 		case "0001111" : switch(fun3)
 						{
 							case "000" : // fence
-								id=7;
+								id=8;
 								type=2;
 								rds= array.substring(7,11);
 								rd=Integer.parseInt(rds,2);
@@ -127,7 +127,7 @@ public class decode {
 								imm=Integer.parseInt(imms,2);
 								
 							case "001" : // fence.i
-								id=8;
+								id=9;
 								type=2;
 								rds= array.substring(7,11);
 								rd=Integer.parseInt(rds,2);
@@ -141,15 +141,6 @@ public class decode {
 		case "0010011" : switch(fun3)
 						{
 							case "000" : // addi
-								id=9;
-								type=2;
-								rds= array.substring(7,11);
-								rd=Integer.parseInt(rds,2);
-								rs1s= array.substring(15,19);
-								rs1=Integer.parseInt(rs1s,2);
-								imms= array.substring(20 ,31);
-								imm=Integer.parseInt(imms,2);
-							case "001" : // slli
 								id=10;
 								type=2;
 								rds= array.substring(7,11);
@@ -158,17 +149,16 @@ public class decode {
 								rs1=Integer.parseInt(rs1s,2);
 								imms= array.substring(20 ,31);
 								imm=Integer.parseInt(imms,2);
-							case "010" : // slti
+							case "001" : // slli
 								id=11;
 								type=2;
 								rds= array.substring(7,11);
 								rd=Integer.parseInt(rds,2);
 								rs1s= array.substring(15,19);
 								rs1=Integer.parseInt(rs1s,2);
-								rs1value=REG[rs1].x;
 								imms= array.substring(20 ,31);
 								imm=Integer.parseInt(imms,2);
-							case "011" : // sltiu
+							case "010" : // slti
 								id=12;
 								type=2;
 								rds= array.substring(7,11);
@@ -178,8 +168,18 @@ public class decode {
 								rs1value=REG[rs1].x;
 								imms= array.substring(20 ,31);
 								imm=Integer.parseInt(imms,2);
-							case "100" : // xori
+							case "011" : // sltiu
 								id=13;
+								type=2;
+								rds= array.substring(7,11);
+								rd=Integer.parseInt(rds,2);
+								rs1s= array.substring(15,19);
+								rs1=Integer.parseInt(rs1s,2);
+								rs1value=REG[rs1].x;
+								imms= array.substring(20 ,31);
+								imm=Integer.parseInt(imms,2);
+							case "100" : // xori
+								id=14;
 								type=2;
 								rds= array.substring(7,11);
 								rd=Integer.parseInt(rds,2);
@@ -191,13 +191,15 @@ public class decode {
 							case "101" : switch(fun7)
 										{
 											case "0000000" : // srli
+												id=15;
 												
 											case "0100000" : // srai
+												id=16;
 												
 										}
 								
 							case "110" : // ori
-								id=16;
+								id=17;
 								type=2;
 								rds= array.substring(7,11);
 								rd=Integer.parseInt(rds,2);
@@ -207,7 +209,7 @@ public class decode {
 								imms= array.substring(20 ,31);
 								imm=Integer.parseInt(imms,2);
 							case "111" : // andi
-								id=17;
+								id=18;
 								type=2;
 								rds= array.substring(7,11);
 								rd=Integer.parseInt(rds,2);
@@ -219,7 +221,7 @@ public class decode {
 						}
 		
 		case "0010111" : // auipc
-			id=18;
+			id=19;
 			type=5; //U
 			rds= array.substring(7,11);
 			rd=Integer.parseInt(rds,2);
@@ -229,7 +231,7 @@ public class decode {
 		case "0011011" : switch(fun3)
 						{
 							case "000" : // addiw
-								id=19;
+								id=20;
 								type=2;
 								rds= array.substring(7,11);
 								rd=Integer.parseInt(rds,2);
@@ -240,7 +242,7 @@ public class decode {
 								imm=Integer.parseInt(imms,2);
 								
 							case "001" :
-								id=20;
+								id=21;
 								type=2;
 								rds= array.substring(7,11);
 								rd=Integer.parseInt(rds,2);
@@ -253,27 +255,16 @@ public class decode {
 							case "101" : switch(fun7)
 										{
 											case "0000000" : // srliw
+												id=22;
 											
 											case "0100000" : // sraiw
+												id=23;
 										}
 						}
 		
 		case "0100011" : switch(fun3)
 						{
 							case "000" : // sb
-								id=23;
-								type=3; //S
-								rs1s= array.substring(15,19);
-								rs1=Integer.parseInt(rs1s,2);
-								rs1value=REG[rs1].x;
-								rs2s=array.substring(20,24);
-								rs2=Integer.parseInt(rs2s,2);
-								rs2value=REG[rs2].x;
-								imms1= array.substring(7 , 11);
-								imms2=array.substring(25 ,31);
-								imms=imms2.concat(imms1);
-								imm=Integer.parseInt(imms,2);
-							case "001" : // sh
 								id=24;
 								type=3; //S
 								rs1s= array.substring(15,19);
@@ -286,7 +277,7 @@ public class decode {
 								imms2=array.substring(25 ,31);
 								imms=imms2.concat(imms1);
 								imm=Integer.parseInt(imms,2);
-							case "010" : // sw
+							case "001" : // sh
 								id=25;
 								type=3; //S
 								rs1s= array.substring(15,19);
@@ -299,8 +290,21 @@ public class decode {
 								imms2=array.substring(25 ,31);
 								imms=imms2.concat(imms1);
 								imm=Integer.parseInt(imms,2);
-							case "011" : // sd
+							case "010" : // sw
 								id=26;
+								type=3; //S
+								rs1s= array.substring(15,19);
+								rs1=Integer.parseInt(rs1s,2);
+								rs1value=REG[rs1].x;
+								rs2s=array.substring(20,24);
+								rs2=Integer.parseInt(rs2s,2);
+								rs2value=REG[rs2].x;
+								imms1= array.substring(7 , 11);
+								imms2=array.substring(25 ,31);
+								imms=imms2.concat(imms1);
+								imm=Integer.parseInt(imms,2);
+							case "011" : // sd
+								id=27;
 								type=2;
 								rds= array.substring(7,11);
 								rd=Integer.parseInt(rds,2);
@@ -318,7 +322,7 @@ public class decode {
 								switch(fun7)
 										{
 											case "0000000" : // add
-												id=27;
+												id=28;
 												type=1; //R
 												rds= array.substring(7,11);
 												rd=Integer.parseInt(rds,2);
@@ -329,7 +333,7 @@ public class decode {
 												rs2=Integer.parseInt(rs2s,2);
 												rs2value=REG[rs2].x;
 											case "0100000" : // sub
-												id=28;
+												id=29;
 												type=1; //R
 												rds= array.substring(7,11);
 												rd=Integer.parseInt(rds,2);
@@ -342,7 +346,7 @@ public class decode {
 										}
 								
 							case "001" : // sll
-								id=29;
+								id=30;
 								type=1; //R
 								rds= array.substring(7,11);
 								rd=Integer.parseInt(rds,2);
@@ -354,17 +358,6 @@ public class decode {
 								rs2value=REG[rs2].x;
 								
 							case "010" : // slt
-								id=30;
-								type=1; //R
-								rds= array.substring(7,11);
-								rd=Integer.parseInt(rds,2);
-								rs1s= array.substring(15,19);
-								rs1=Integer.parseInt(rs1s,2);
-								rs1value=REG[rs1].x;
-								rs2s= array.substring(20,24);
-								rs2=Integer.parseInt(rs2s,2);
-								rs2value=REG[rs2].x;
-							case "011" : // sltu
 								id=31;
 								type=1; //R
 								rds= array.substring(7,11);
@@ -375,8 +368,19 @@ public class decode {
 								rs2s= array.substring(20,24);
 								rs2=Integer.parseInt(rs2s,2);
 								rs2value=REG[rs2].x;
-							case "100" : // xor
+							case "011" : // sltu
 								id=32;
+								type=1; //R
+								rds= array.substring(7,11);
+								rd=Integer.parseInt(rds,2);
+								rs1s= array.substring(15,19);
+								rs1=Integer.parseInt(rs1s,2);
+								rs1value=REG[rs1].x;
+								rs2s= array.substring(20,24);
+								rs2=Integer.parseInt(rs2s,2);
+								rs2value=REG[rs2].x;
+							case "100" : // xor
+								id=33;
 								type=1; //R
 								rds= array.substring(7,11);
 								rd=Integer.parseInt(rds,2);
@@ -389,7 +393,7 @@ public class decode {
 							case "101" :  switch(fun7)
 										{
 											case "0000000" : // srl
-												id=33;
+												id=34;
 												type=1; //R
 												rds= array.substring(7,11);
 												rd=Integer.parseInt(rds,2);
@@ -400,7 +404,7 @@ public class decode {
 												rs2=Integer.parseInt(rs2s,2);
 												rs2value=REG[rs2].x;
 											case "0100000" :// sra
-												id=34;
+												id=35;
 												type=1; //R
 												rds= array.substring(7,11);
 												rd=Integer.parseInt(rds,2);
@@ -414,7 +418,7 @@ public class decode {
 							
 								
 							case "110" : // or
-								id=35;
+								id=36;
 								type=1; //R
 								rds= array.substring(7,11);
 								rd=Integer.parseInt(rds,2);
@@ -425,7 +429,7 @@ public class decode {
 								rs2=Integer.parseInt(rs2s,2);
 								rs2value=REG[rs2].x;
 							case "111" : // and
-								id=36;
+								id=37;
 								type=1; //R
 								rds= array.substring(7,11);
 								rd=Integer.parseInt(rds,2);
@@ -438,7 +442,7 @@ public class decode {
 						}
 		
 		case "0110111" : // lui
-			id=37;
+			id=38;
 			type=5; //U
 			rds= array.substring(7,11);
 			rd=Integer.parseInt(rds,2);
@@ -450,7 +454,7 @@ public class decode {
 							case "000" : switch(fun7)
 										{
 											case "0000000" : // addw
-												id=38;
+												id=39;
 												type=1; //R
 												rds= array.substring(7,11);
 												rd=Integer.parseInt(rds,2);
@@ -461,7 +465,7 @@ public class decode {
 												rs2=Integer.parseInt(rs2s,2);
 												rs2value=REG[rs2].x;
 											case "0100000" : // subw
-												id=39;
+												id=40;
 												type=1; //R
 												rds= array.substring(7,11);
 												rd=Integer.parseInt(rds,2);
@@ -474,7 +478,7 @@ public class decode {
 										}
 			
 							case "001" : // sllw
-								id=40;
+								id=41;
 								type=1; //R
 								rds= array.substring(7,11);
 								rd=Integer.parseInt(rds,2);
@@ -487,7 +491,7 @@ public class decode {
 							case "101" : switch(fun7)
 										{
 											case "0000000" : // srlw
-												id=41;
+												id=42;
 												type=1; //R
 												rds= array.substring(7,11);
 												rd=Integer.parseInt(rds,2);
@@ -498,7 +502,7 @@ public class decode {
 												rs2=Integer.parseInt(rs2s,2);
 												rs2value=REG[rs2].x;
 											case "0100000" : // sraw
-												id=42;
+												id=43;
 												type=1; //R
 												rds= array.substring(7,11);
 												rd=Integer.parseInt(rds,2);
@@ -514,7 +518,7 @@ public class decode {
 		case "1100011" : switch(fun3)
 						{
 							case "000" : // beq
-								id=43;
+								id=44;
 								type=4;
 								rs1s= array.substring(15,19);
 								rs1=Integer.parseInt(rs1s,2);
@@ -530,18 +534,21 @@ public class decode {
 								String t4=temp2.substring(0); //12
 								String t5=t4.concat(t3); 
 							case "001" : // bne
+								id=45;
 								
 							case "100" : // blt
+								id=46;
 								
 							case "101" : // bge
-								
+								id=47;
 							case "110" : // bltu
-								
+								id=48;
 							case "111" : // bgeu
+								id=49;
 						}
 		
 		case "1100111" : // jalr
-			id=49;
+			id=50;
 			type=2;
 			rds= array.substring(7,11);
 			rd=Integer.parseInt(rds,2);
@@ -551,13 +558,13 @@ public class decode {
 			imms= array.substring(20 ,31);
 			imm=Integer.parseInt(imms,2);	
 		case "1101111" : // jal
-			
+			id=51;
 		case "1110011" :switch(fun3)
 						{
 							case "000" : switch(immidiate)
 										{
 											case "000000000000" : // ecall
-												id=51;
+												id=52;
 												type=2;
 												rds= array.substring(7,11);
 												rd=Integer.parseInt(rds,2);
@@ -565,7 +572,7 @@ public class decode {
 												rs1=Integer.parseInt(rs1s,2);
 												rs1value=REG[rs1].x;
 											case "000000000001" : // ebreak
-												id=52;
+												id=53;
 												type=2;
 												rds= array.substring(7,11);
 												rd=Integer.parseInt(rds,2);
@@ -575,26 +582,6 @@ public class decode {
 										}
 								
 							case "001" : // CSRRW
-								id=52;
-								type=2;
-								rds= array.substring(7,11);
-								rd=Integer.parseInt(rds,2);
-								rs1s= array.substring(15,19);
-								rs1=Integer.parseInt(rs1s,2);
-								rs1value=REG[rs1].x;
-								imms= array.substring(20 ,31);
-								imm=Integer.parseInt(imms,2);
-							case "010" : // CSRRS
-								id=53;
-								type=2;
-								rds= array.substring(7,11);
-								rd=Integer.parseInt(rds,2);
-								rs1s= array.substring(15,19);
-								rs1=Integer.parseInt(rs1s,2);
-								rs1value=REG[rs1].x;
-								imms= array.substring(20 ,31);
-								imm=Integer.parseInt(imms,2);
-							case "011" : // CSRRC
 								id=54;
 								type=2;
 								rds= array.substring(7,11);
@@ -604,8 +591,28 @@ public class decode {
 								rs1value=REG[rs1].x;
 								imms= array.substring(20 ,31);
 								imm=Integer.parseInt(imms,2);
-							case "101" : // CSRRWI
+							case "010" : // CSRRS
 								id=55;
+								type=2;
+								rds= array.substring(7,11);
+								rd=Integer.parseInt(rds,2);
+								rs1s= array.substring(15,19);
+								rs1=Integer.parseInt(rs1s,2);
+								rs1value=REG[rs1].x;
+								imms= array.substring(20 ,31);
+								imm=Integer.parseInt(imms,2);
+							case "011" : // CSRRC
+								id=56;
+								type=2;
+								rds= array.substring(7,11);
+								rd=Integer.parseInt(rds,2);
+								rs1s= array.substring(15,19);
+								rs1=Integer.parseInt(rs1s,2);
+								rs1value=REG[rs1].x;
+								imms= array.substring(20 ,31);
+								imm=Integer.parseInt(imms,2);
+							case "101" : // CSRRWI
+								id=57;
 								type=2;
 								rds= array.substring(7,11);
 								rd=Integer.parseInt(rds,2);
@@ -615,7 +622,7 @@ public class decode {
 								imms= array.substring(20 ,31);
 								imm=Integer.parseInt(imms,2);
 							case "110" : // CSRRSI
-								id=55;
+								id=58;
 								type=2;
 								rds= array.substring(7,11);
 								rd=Integer.parseInt(rds,2);
@@ -625,7 +632,7 @@ public class decode {
 								imms= array.substring(20 ,31);
 								imm=Integer.parseInt(imms,2);
 							case "111" : // CSRRCI
-								id=56;
+								id=59;
 								type=2;
 								rds= array.substring(7,11);
 								rd=Integer.parseInt(rds,2);
@@ -637,6 +644,36 @@ public class decode {
 						}
 		
 		}
+	}
+
+
+	public int typereturn() {
+		// TODO Auto-generated method stub
+		return type;
+	}
+
+
+	public int rs1return() {
+		// TODO Auto-generated method stub
+		return rs1value;
+	}
+
+
+	public int rs2return() {
+		// TODO Auto-generated method stub
+		return rs2value;
+	}
+
+
+	public int rdreturn() {
+		// TODO Auto-generated method stub
+		return rd;
+	}
+
+
+	public int idreturn() {
+		// TODO Auto-generated method stub
+		return id;
 	}
 	
 }
