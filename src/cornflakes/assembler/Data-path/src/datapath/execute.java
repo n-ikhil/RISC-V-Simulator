2d4bf853bafcae60014901cc31cdc340dd26d609
yashaswi2000 <31373501+yashaswi2000@users.noreply.github.com>
2019-03-21 16:43:40 +0530

    added R-type execute

--- src/cornflakes/assembler/Data-path/src/datapath/execute.java
+++ src/cornflakes/assembler/Data-path/src/datapath/execute.java
@@ -6,7 +6,97 @@ public class execute {
 	int rs2value;
 	int rd;
 	int id;
+	int output;
 	
+	execute(int id0,int rd,int rs1value,int rs2value)  // R-type constructor
+	{
+		id = id0;
+		switch(id)
+		{
+		case 28 : // add
+			//System.out.print("yesss");
+			output = rs1value + rs2value;
+			break;
+			
+		case 29 : // sub
+			output = rs1value - rs2value;
+			break;
+			
+		case 30 : // sll
+			output = rs1value << rs2value;
+			break;
+			
+		case 31 : // slt
+			if(rs1value < rs2value)
+			{
+				output = 1;
+			}
+			else
+			{
+				output = 2;
+			}
+			break;
+			
+		case 32 : //sltu
+			if(rs1value < rs2value)
+			{
+				output = 1;
+			}
+			else
+			{
+				output = 2;
+			}
+			break;
+			
+		case 33 : //xor
+			output = rs1value^rs2value;
+			break;
+			
+		case 34 : //srl
+			output = rs1value >>> rs2value;
+			break;
+			
+		case 35 : // sra
+			output = rs1value >> rs2value;
+			break;
+			
+		case 36 : // or
+			output = rs1value | rs2value;
+			break;
+			
+		case 37 : // and
+			output = rs1value & rs2value;
+			break;
+			
+			
+		case 39 : // addw
+			output = rs1value + rs2value;
+			break;
+			
+		case 40 : // subw
+			output = rs1value - rs2value;
+			break;
+			
+		case 41 : // sllw
+			output = rs1value << rs2value;
+			break;
+			
+		case 42 : //srlw
+			output = rs1value >>> rs2value;
+			break;
+			
+		case 43 : //sraw
+			output = rs1value >> rs2value;
+			break;
+			
+			
+			
+			
+		}
+		
+		
+		
+	}
 	
 	
 	
