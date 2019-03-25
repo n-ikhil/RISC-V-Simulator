53f9ccd30c145df042b417347b672d183dfff5a9
yashaswi2000 <31373501+yashaswi2000@users.noreply.github.com>
2019-03-26 00:55:52 +0530

    new datapath

--- src/cornflakes/assembler/Data-path/src/datapath/datapath.java
+++ src/cornflakes/assembler/Data-path/src/datapath/datapath.java
@@ -1,115 +1,61 @@
-package datapath;
-
-import datapath.register;
-import datapath.decode;
-import datapath.execute;
-
 import java.util.*;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner; 
 
 public class datapath {
-	static register REG[];
-	
-	
-	public static void main(String Args[]) throws Exception
+
+	static boolean check(String inp,execution[] ex)
 	{
-		File file = new File("/home/yashaswi/yash.txt");
-		Scanner sc =  new Scanner(file);
-		System.out.print("sdfjsgd\n");
-		
-		REG = new register[32];
-		for(int h=0;h<32;h++)
-		{
-			register reg = new register();
-			reg.x = 0;
-			REG[h] = reg;
-		}
-		Vector v = new Vector();
-		
-		while (sc.hasNextLine())
-		{
-			System.out.print("sdfjsgd\n");
-			String mcode = new String();
-			mcode  = sc.nextLine();
-			v.add(mcode);
+		//System.out.println("< "+inp+" >"+" "+ex.length);
+		String temp_comp="00000000000000000000000000000000";
+		if( !inp.equals(temp_comp) ) return true;
+		for(execution ui:ex)
+		{ 
+			if(ui==null) continue;
+			if(!ui.retired)return true;
 		}
 		
-		int k = 0;
-		int length = v.size();
-		//System.out.print(length);
+		return false;
+	}
+	
+	
+	
+	public static void run(primary_memory mem,boolean piplined)
+	{
+		mem.pc=0;
+		execution[] ar = new execution[6];
 		
-		while(k<length)
+
+		mem.register[1]=10;
+		mem.register[2]=20;
+		mem.register[3]=30;
+ 
+		while(check(mem.loadword(mem.pc),ar))
 		{
-			String mcode1 = new String();
-			mcode1 = (String) v.get(k);
-			//System.out.print(mcode1.length());
-			decode D = new decode(mcode1,REG);
-			int type = D.typereturn();
-			//System.out.print(type);
-			switch(type)
+			String instr=mem.loadword(mem.pc);
+			mem.ir=instr;mem.pc=mem.pc+4;//stage 1
+			ar[0]=new execution(mem,piplined);//if instr=="000000000" retired=true
+
+			for(int instr_stg=0;instr_stg<5;instr_stg++)
 			{
-			case 1 :                                                       //R
-				int rs1 = D.rs1return();
-				int rs2 = D.rs2return();
-				int rd = D.rdreturn();
-				int id = D.idreturn();
-				execute ex1 = new execute(id,rd,rs1,rs2);
-				int out = ex1.output;
-				REG[rd].x = out;
-					
-				break;
 				
-			case 2 :                                                      //I
-				int rs1 = D.rs1return();
-				int rs2 = D.rs2return();
-				int id  = D.idreturn();
-				int imm = D.immreturn();	
-		                execute ex2 = new executei(id,rs1,imm);	
-				int out = ex2.output;
-				
-				break;	
-				
-			case 3 :                                                     //S
-		                int rs1 = D.rs1return();
-				int id = D.idreturn();
-				int imm = D.immreturn();
-				execute ex3 = new executes(id,rs1,imm);	
-				int out = ex3.output;
-				
-				break;
-					
-			case 4 : //SB
-				int rs1 = D.rs1return();
-				int rs2 =D.rs2return();
-				int id = D.idreturn();
-				int imm = D.immreturn();
-				execute ex4 = new executesb(id,rs1,rs2,imm);	
-				int out = ex4.output;
-				
-				break;
-				
-			case 5 :                                                    //UJ
-				int rs1 = D.rs1return();
-				int id = D.idreturn();
-				int imm = D.immreturn();
-				execute ex5 = new executeuj(id,rs1,imm);	
-				int out = ex5.output;
-				
-				break;
-				
-			case 6 :                                                    //U
-				int rs1 = D.rs1return();
-				int id = D.idreturn();
-				int imm = D.immreturn();
-				execute ex6 = new executeu(id,rs1,imm);	
-				int out = ex6.output;
-				
-				break;
+				 if(ar[instr_stg]==null) continue;
+				 if(ar[instr_stg].retired) continue;
+				 
+				 ar[instr_stg].run(mem);
+				 
+			
 			}
-			k++;
+			for(int shiftbyone=4;shiftbyone>=0;shiftbyone--)
+			{
+				ar[shiftbyone+1]=ar[shiftbyone];
+			}
+
+
 		}
+		for(int uio:mem.register)
+			System.out.println(uio);
 	}
 
 }
